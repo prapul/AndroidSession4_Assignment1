e8a38bc8b5366276d1422830ff72181c2a8838c7
prapul <prapulpoonacha@gmail.com>
2016-12-26 16:38:08 +0530

    protected method from different package

---
+++ AndroidSession4_Assignment1_1.java
@@ -0,0 +1,7 @@
+package prapul;
+import sample.*;
+public class AndroidSession4_Assignment1_1 extends AndroidSession4_Assignment1_2{
+	public static void main(String args[]){
+		print();
+	}
+}
---
+++ AndroidSession4_Assignment1_2.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class AndroidSession4_Assignment1_2 {
+	public static void main(String args[]){
+		print();
+	}
+	protected static void print(){
+		System.out.println("Hello world!");
+	}
+}
